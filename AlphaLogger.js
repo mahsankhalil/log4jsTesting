254d3226c9972b95aeaec28b0f0ae9048e90d0b9
Ahsan Khalil <mahsanprogrammer@gmail.com>
2021-06-29 01:33:19 +0500

    basic wrapper implementation of Log4js

--- AlphaLogger.js
+++ AlphaLogger.js
@@ -1,12 +1,16 @@
 const log4js = require('log4js')
 
 module.exports = class AlphaLogger {
-    static shared = AlphaLogger();
-    logger = null
+    static shared = new AlphaLogger();
+    infoLogger = null
+    errorLogger = null
+    warningLogger = null
     constructor() {
         const log4js = require('log4js');
-        log4js.configure('./config/log4js.json');
+        log4js.configure('./log4js.json');
         //var logger = log4js.getLogger(); //for both console and file
-        self.logger = log4js.getLogger("file");
+        this.infoLogger = log4js.getLogger("Info");
+        this.warningLogger = log4js.getLogger("Warning");
+        this.errorLogger = log4js.getLogger("Error");
     }
 }
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const log4js = require("log4js");
-log4js.configure('./log4js.json');
+// const log4js = require("log4js");
+// log4js.configure('./log4js.json');
 //var logger = log4js.getLogger(); //for both console and file
-const logger = log4js.getLogger("default");
-logger.debug("fucking error")
+// const warningLogger = log4js.getLogger("Warning");
+// const infoLogger = log4js.getLogger("Info");
+// const errorLogger = log4js.getLogger("Error");
+//
+// warningLogger.info("Warning aa gai")
+// infoLogger.info("Warning aa gai")
+// errorLogger.error("error aa gaya")
+
+const alphaLogger = require('./AlphaLogger')
+alphaLogger.shared.errorLogger.error("Error is about to come");
+alphaLogger.shared.warningLogger.warn("Warning is about to come");
+alphaLogger.shared.infoLogger.warn("Info is about to come");
